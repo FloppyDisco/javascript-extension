e33cc570a7a39d6bd8043a599c47f7b6b0f9e3c2
FloppyDisco <joshuapaulpiasecki@gmail.com>
2024-08-20 10:16:17 +0200

    created initial jump function

--- extension.js
+++ extension.js
@@ -1,36 +1,213 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "javascript-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('javascript-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from javascript-extension!');
+  const insertCursorBeforeDefault = vscode.workspace
+    .getConfiguration()
+    .get("frogger.insertCursorBeforeByDefault", false);
+  context.globalState.update(
+    "insertCursorBefore",
+    insertCursorBeforeDefault
+  );
+
+  // context.globalState.update('setting', newState);
+  // context.globalState.get('setting', false);
+
+  // create variables for the following
+
+  /*
+		highlightToMatch     		cmd+l
+		insertCursorBeforeMatch   	cmd+i
+		searchBackwards      		cmd+k
+		searchFromTop				cmd+t
+		searchFromBottom			cmd+b
+	*/
+
+  /*
+	 settings
+
+		searchBackwardsIfNoMatch
+		insertCursorBeforeMatchByDefault
+		wrapSearch
+
+	*/
+
+  // |-------------------------|
+  // |        create UI        |
+  // |-------------------------|
+
+  const inputBox = vscode.window.createInputBox();
+  inputBox.placeholder = "jump to ...";
+  inputBox.prompt = "jumping";
+  inputBox.title = "🐸";
+  inputBox.buttons = [
+    {
+      iconPath: new vscode.ThemeIcon("triangle-left"),
+      tooltip: "Search",
+    },
+    {
+      iconPath: new vscode.ThemeIcon("close"),
+      tooltip: "Cancel",
+    },
+  ];
+
+  /*
+		add commands to start these functions with specific settings, regardless of state or defaults
+
+		- command to jump to the typed letter
+			- "frogger.jump"
+		- command to jump to the next occurence of the prev search
+			- "frogger.jumpToNext"
+		- command to jump to the prev occurence of the prev search
+			- "frogger.jumpToPrev"
+		- command to jump with highlighting already selected
+			- "frogger.jumpSelect"
+		- command to jump with backwards already selected
+			- "frogger.jumpBackwards"
+		- command to jump with insertCursorBeforeMatch set to true
+			- "frogger.jumpTo"
+		- command to jump with insertCursorBeforeMatch set to false
+			- "frogger.jumpAfter"
+		- command to jump to first occurence from top of file
+			- "frogger.jumpFromTop"
+		- command to jump to first occurence from bottom of file
+			- "frogger.jumpFromBottom"
+		- command to toggle all variables
+	*/
+
+  let disposable = vscode.commands.registerCommand("frogger.jump", () => {
+
+	const insertCursorBefore = context.globalState.get('insertCursorBefore', insertCursorBeforeDefault);
+		jump(
+			inputBox,
+			insertCursorBefore,
+		)
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function jump(
+	inputBox,
+	insertCursorBefore,
+) {
+  inputBox.show();
+
+  // Handle when the user types in the input box
+  inputBox.onDidChangeValue((value) => {
+    if (value.length > 0) {
+      vscode.window.showInformationMessage(`froggering to: ${value}`);
+      searchInEditor(value, insertCursorBefore);
+      inputBox.value = "";
+      inputBox.hide();
+    }
+    inputBox.hide();
+  });
+  inputBox.onDidTriggerButton((button) => {
+    switch (button.tooltip) {
+      case "Trigger Command":
+        // Call a command when the "Trigger Command" button is clicked
+        vscode.commands.executeCommand("extension.someCommand");
+        break;
+      case "Cancel":
+        // Hide the input box when the "Cancel" button is clicked
+        inputBox.hide();
+        break;
+      default:
+        // Handle any other cases if necessary
+        break;
+    }
+  });
+
+  inputBox.onDidAccept(() => {
+    inputBox.hide();
+  });
+  inputBox.onDidHide(() => {
+    inputBox.hide();
+  });
+}
+
+function searchInEditor(searchTerm, insertCursorBeforeMatch) {
+  // store the searchTerm in global state
+
+  const editor = vscode.window.activeTextEditor;
+  if (editor && searchTerm) {
+    const document = editor.document;
+
+    const text = document.getText();
+
+    // escape any regex special characters
+    searchTerm = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+    const searchTermRegex = new RegExp(searchTerm, "g");
+
+    const cursorPosition = editor.selection.active;
+    const cursorOffset = document.offsetAt(cursorPosition);
+
+    const allMatches = [];
+    let match;
+    while ((match = searchTermRegex.exec(text)) !== null) {
+      const startPos = document.positionAt(match.index);
+      const endPos = document.positionAt(match.index + match[0].length);
+      allMatches.push(new vscode.Range(startPos, endPos));
+    }
+
+    const matchesBeforeCursor = allMatches.filter(
+      (range) => document.offsetAt(range.start) < cursorOffset
+    );
+    const matchesAfterCursor = allMatches.filter(
+      (range) => document.offsetAt(range.start) >= cursorOffset
+    );
+
+    // highlight all matches
+    const matchesHighlight = vscode.window.createTextEditorDecorationType({
+      backgroundColor: "rgba(0,255,0,0.4)", //green
+    });
+    editor.setDecorations(matchesHighlight, allMatches);
+
+    let targetMatch;
+
+    if (matchesAfterCursor.length > 0) {
+      targetMatch = matchesAfterCursor[0];
+    } else {
+
+		const WrapSearch = vscode.workspace
+        .getConfiguration()
+        .get("frogger.wrapSearch", false);
+
+      if (WrapSearch && matchesBeforeCursor.length > 0) {
+        targetMatch = matchesBeforeCursor[0];
+      } else {
+
+        const SearchBackwardsIfNoMatch = vscode.workspace
+          .getConfiguration()
+          .get("frogger.searchBackwardsIfNoMatch", false);
+		  
+        if (SearchBackwardsIfNoMatch && matchesBeforeCursor.length > 0) {
+          targetMatch = matchesBeforeCursor[matchesBeforeCursor.length - 1];
+        }
+      }
+    }
+
+    // modify this function to accept a highlighting feature
+    if (targetMatch) {
+      const newPosition = insertCursorBeforeMatch ? targetMatch.start : targetMatch.end;
+
+      // change this selection to include the old position if highlighting
+      // if jumping with a previous search the old position should be passed from the original search
+      // this will need to be stored in state
+      editor.selection = new vscode.Selection(newPosition, newPosition);
+
+      // make the reveal location editable from settings
+      editor.revealRange(targetMatch, vscode.TextEditorRevealType.InCenter);
+    }
+  }
+}
+
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
